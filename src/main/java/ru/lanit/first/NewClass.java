6754d5a5a87102698efce02b23593eaef703c5be
AKusakin <anatolii.kusakin1234@gmail.com>
2019-08-29 13:52:49 +0500

    29.5 commit

--- src/main/java/ru/lanit/first/NewClass.java
+++ src/main/java/ru/lanit/first/NewClass.java
@@ -3,16 +3,10 @@ package ru.lanit.first;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewClass extends MatrixReader {
+public class NewClass {
     public void Hello() {
         System.out.println("Hello");
     }
 
-    @Override
-    public List<Matrix> readMatrix(String filepath) throws Exception {
-        List<Matrix> matrices = new ArrayList<Matrix>();
-        System.out.println("readMatrix");
-        return matrices;
-    }
 
 }
