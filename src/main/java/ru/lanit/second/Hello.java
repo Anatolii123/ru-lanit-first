7cf1b205188cccf0c8910e699a9bce4f33349937
AKusakin <anatolii.kusakin1234@gmail.com>
2019-08-30 15:41:03 +0500

    30.15 commit

--- src/main/java/ru/lanit/second/Hello.java
+++ src/main/java/ru/lanit/second/Hello.java
@@ -7,36 +7,6 @@ import java.util.List;
 
 public class Hello {
     public static void main (String[] args) throws Exception {
-//        Matrix matrixA = new Matrix(); //new Matrix(3,3);
-//        matrixA.set();
-//        matrixA.transpose();
-//        Matrix matrixB = new Matrix();
-//        matrixB.set();
-//        matrixB.transpose();
-//
-//        MatrixSummator matrixS = new MatrixSummator();
-//        matrixS.sum(matrixA);
-//        MatrixMultiplicator matrixM = new MatrixMultiplicator();
-//        matrixM.mult(matrixB);
-
-//        MatrixReader matrixR = new MatrixReader();
-//        List<Matrix> matrices = matrixR.readMatrix("D:\\matrix1.txt");
-//        MatrixSummator sum = new MatrixSummator();
-//
-//
-//        //Цикл по матрицам из списка. Ессли sum пуст, то вызываем set, параметры для него берём из
-//        for(Matrix matrix : matrices){
-//            if (sum.isEmpty()) {
-//                sum.setMatrix(matrix.getMatrix());
-//                sum.setA(matrix.getMatrix().length);
-//                sum.setB(matrix.getMatrix()[0].length);
-//                continue;
-//            }
-//            sum = sum.sum(matrix);
-//            System.out.println();
-//            System.out.println(matrix.hashCode());
-//        }
-//        sum.showMessage();
 
         for (Description d: Description.values()) {
             if (d.equals(d)){
@@ -50,14 +20,6 @@ public class Hello {
         System.out.println(Description.getBy(false));
         System.out.println();
 
-        //ElectricField e = new ElectricField();
-        //ElectricField e2 = ElectricField.getInstanceOf();
-
-//        m.science = Description.Science.getDescription();
-//        System.out.println(m.science);
-//        m.science = Description.Science.toString();
-//        System.out.println(Matter.getInstanceOf().science);
-//        System.out.println(Description.constant());
 
 
     }
