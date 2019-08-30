da873241e203135b2507726ad7e5939606cd6559
AKusakin <anatolii.kusakin1234@gmail.com>
2019-08-30 16:09:50 +0500

    30.16 commit

--- src/main/java/ru/lanit/first/BaryonicMatter.java
+++ src/main/java/ru/lanit/first/BaryonicMatter.java
@@ -24,4 +24,25 @@ public class BaryonicMatter extends HadronSubstance {
         BaryonicMatter b = new BaryonicMatter();
         return b;
     }
+
+    class BaryonicMatter2 extends Substance {
+
+        @Override
+        public void printClass() {
+            System.out.println("Baryonic Matter");
+        }
+
+        @Override
+        public void printAncestor() {
+            System.out.println("Substance");
+            super.printAncestor();
+        }
+
+        @Override
+        public void printDescendants() {
+            System.out.println("Classical Substance");
+        }
+
+    }
+
 }
--- src/main/java/ru/lanit/second/Hello.java
+++ src/main/java/ru/lanit/second/Hello.java
@@ -1,8 +1,10 @@
 package ru.lanit.second;
 
+import ru.lanit.first.BaryonicMatter;
+import ru.lanit.first.ClassicalSubstance;
 import ru.lanit.first.Description;
-import ru.lanit.first.ElectricField;
-
+import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hello {
@@ -19,7 +21,21 @@ public class Hello {
         System.out.println(Description.getBy(10));
         System.out.println(Description.getBy(false));
         System.out.println();
-
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(2);
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        List<Integer> unmodifiableList = Collections.unmodifiableList(list);
+        System.out.println(unmodifiableList);
+        List<Integer> list2 = Collections.unmodifiableList(new ArrayList<Integer>() {{
+            add(2);
+            add(3);
+            add(5);
+            add(7);
+        }});
+        System.out.println(list2);
+        BaryonicMatter b = BaryonicMatter.getInstanceOf();
 
 
     }
