c2b1defc25813335047cc23b97a79296b578e725
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-02 10:54:56 +0500

    02.09.02 commit

--- src/main/java/ru/lanit/first/Matter.java
+++ src/main/java/ru/lanit/first/Matter.java
@@ -2,34 +2,28 @@ package ru.lanit.first;
 
 import java.util.Scanner;
 
-public class Matter {
+abstract class Matter {
 
     protected String science;
     protected boolean mathDescription;
 
     protected Matter(){}
 
-    public void printClass() {
-        System.out.println("Matter");
-    }
-    public void printAncestor() {
-        System.out.println("Object");
-    }
-    public void printDescendants() {
-        System.out.println(MatterDescendants.values());
-    }
+    public abstract void printClass();
+    public abstract void printAncestor();
+    public abstract void printDescendants();
 
-    public static Matter getInstanceOf(){
-        Matter m = new Matter();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите название науки: ");
-        m.science = in.nextLine();
-        System.out.println("Можно ли математически описать: ");
-        if (in.nextLine() == "Да") {
-            m.mathDescription = true;
-        }
-        m.mathDescription = false;
-        return m;
-    }
+//    public static Matter getInstanceOf(){
+//        Matter m = new Matter();
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите название науки: ");
+//        m.science = in.nextLine();
+//        System.out.println("Можно ли математически описать: ");
+//        if (in.nextLine() == "Да") {
+//            m.mathDescription = true;
+//        }
+//        m.mathDescription = false;
+//        return m;
+//    }
 }
 
