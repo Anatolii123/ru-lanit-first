076fe64e582d9078abaf91d1fc2cac3fe5e36df3
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-02 11:36:05 +0500

    02.09.07 commit

--- src/main/java/ru/lanit/first/ObscurePhysicalNature.java
+++ src/main/java/ru/lanit/first/ObscurePhysicalNature.java
@@ -4,6 +4,22 @@ public class ObscurePhysicalNature extends Matter {
 
     protected ObscurePhysicalNature(){}
 
+    public void mathDescription() {
+        System.out.println("It has not.");
+    }
+
+    public void force() {
+        System.out.println("It has.");
+    }
+
+    public void reason() {
+        System.out.println("Unknown.");
+    }
+
+    public void amountOfInformation() {
+        System.out.println("A few of!");
+    }
+
     @Override
     public void printClass() {
         System.out.println("Obscure Physical Nature");
@@ -12,7 +28,6 @@ public class ObscurePhysicalNature extends Matter {
     @Override
     public void printAncestor() {
         System.out.println("Matter");
-        super.printAncestor();
     }
 
     @Override
