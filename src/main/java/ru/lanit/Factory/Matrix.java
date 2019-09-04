5ca4e7e65f69cd23720be17724cbeda829229db5
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-04 18:46:14 +0500

    FactoryMethodCommit2

--- src/main/java/ru/lanit/Factory/Matrix.java
+++ src/main/java/ru/lanit/Factory/Matrix.java
@@ -2,5 +2,16 @@ package ru.lanit.Factory;
 
 public class Matrix implements Operations {
 
+    public Operations add(){
+
+    }
+
+    public Operations sub(){
+
+    }
+
+    public Operations mult(){
+
+    }
 
 }
--- src/main/java/ru/lanit/Factory/Operations.java
+++ src/main/java/ru/lanit/Factory/Operations.java
@@ -1,5 +1,7 @@
 package ru.lanit.Factory;
 
 public interface Operations {
-
+    public Operations add();
+    public Operations sub();
+    public Operations mult();
 }
