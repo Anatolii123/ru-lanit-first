1fcb8c0bd747bdbf63989b4e3ddc27dd5ff63483
Anatolii123 <anatolii.kusakin1234@gmail.com>
2019-09-05 09:42:32 +0500

    HomeCommit5

--- src/main/java/ru/lanit/factory/Matrix.java
+++ src/main/java/ru/lanit/factory/Matrix.java
@@ -9,8 +9,34 @@ public class Matrix {
     private String filepath;
     private Operations[][] matrix;
 
+    public Matrix(Matrix source){ // конструктор
+        setA(source.getA());
+        setB(source.getB());
+        setMatrix(source.getMatrix());
+    }
 
-    public void set () {
+    public int getA() {
+        return a;
+    }
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public Operations[][] getMatrix() {
+        return matrix;
+    }
+    public void setMatrix(Operations[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public void set (int row, int column, Operations operations) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите количество строк: ");
         a = in.nextInt();
@@ -18,12 +44,7 @@ public class Matrix {
         b = in.nextInt();
 
         matrix = new Operations[a][b];
-        for(int i = 0; i < a; i++) {
-            for(int j = 0; j < b; j++) {
-                matrix[i][j] = Operations();
-            }
-            System.out.println();
-        }
+        matrix[row][column] = operations;
     }
 
 }
