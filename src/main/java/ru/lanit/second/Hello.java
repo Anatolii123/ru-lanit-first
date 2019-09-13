c1ce82cd963015b9016320040d66fa4bd75b816f
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-13 13:34:11 +0500

    NewMathDependency2Commit

--- src/main/java/ru/lanit/second/Hello.java
+++ src/main/java/ru/lanit/second/Hello.java
@@ -1,17 +1,7 @@
 package ru.lanit.second;
 
-
 import org.apache.commons.math3.linear.Array2DRowFieldMatrix;
 import org.apache.commons.math3.util.BigReal;
-import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
-import org.apache.commons.math3.ml.clustering.Cluster;
-import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 
 public class Hello {
 
@@ -44,58 +34,8 @@ public class Hello {
         showMatrix(matrix6);
 
         System.out.println();
-        doSolve();
-        kMeansClustering();
-    }
-
-    public static void doSolve() {
-        DerivativeStructure x = new DerivativeStructure(1, 3, 0, 2.5);
-        System.out.println("x = " + x.getValue());
-        DerivativeStructure x2 = x.pow(2);
-        DerivativeStructure y = new DerivativeStructure(4.0, x2, 2.0, x); //y = 4x^2 + 2x
-        System.out.println("y = 4x^2 + 2x");
-        System.out.println("y = " + y.getValue());              // y    = 30.0
-        System.out.println("y' = " + y.getPartialDerivative(1));
-        System.out.println("y'' = " + y.getPartialDerivative(2));
-        System.out.println("y''' = " + y.getPartialDerivative(3));
-
-        DerivativeStructure z = new DerivativeStructure(1, 2, 0, 90);
-        z.cos();
 
     }
-
-    public static void kMeansClustering() {
-        int[][] data = {
-                {1,1},{1,2},{1,3},{1,4},{1,5},{2,1},{2,2},{2,3},{2,4},{3,1},{3,2},{3,3},{4,1},{4,2},{5,1},
-                {6,2},{6,3},{6,4},{6,5},{6,6},{5,3},{5,4},{5,5},{5,6},{4,4},{4,5},{4,6},{3,5},{3,6},{2,6}
-        };
-        List<int[][]> data3 = Collections.singletonList(data);
-        int clustersCount = 2;
-        int[][] centroids = new int[][]{{0,4},{5,7}};
-        List<int[][]> data4 = Collections.singletonList(centroids);
-        Cluster cluster1 = new Cluster() {
-            @Override
-            public String toString() {
-                return "Cluster 1";
-            }
-        };
-        Cluster cluster2 = new Cluster() {
-            @Override
-            public String toString() {
-                return "Cluster 2";
-            }
-        };
-        for (int i=0;i<clustersCount;i++) {
-            for (int j=0;j<data.length;j++) {
-
-            }
-        }
-
-        KMeansPlusPlusClusterer kMeansPlusPlusClusterer = new KMeansPlusPlusClusterer(2,100);
-        kMeansPlusPlusClusterer.cluster(data3);
-    }
-
-
 
     public static void showMatrix(Array2DRowFieldMatrix<BigReal> matrix) {
         for(int i = 0; i < 3; i++) {
