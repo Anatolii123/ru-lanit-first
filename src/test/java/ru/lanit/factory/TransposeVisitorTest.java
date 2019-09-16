bbf535c0c4309a315554b17f3b1564190d014388
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-16 17:46:28 +0500

    UnitTest29Commit

--- src/test/java/ru/lanit/factory/TransposeVisitorTest.java
+++ src/test/java/ru/lanit/factory/TransposeVisitorTest.java
@@ -1,5 +1,6 @@
 package ru.lanit.factory;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +9,30 @@ public class TransposeVisitorTest {
 
     @Test
     public void visit() {
+        // arrange
+        TransposeVisitor transposeVisitor = new TransposeVisitor();
+        Matrix a = new Matrix();
+        a.setMatrix(new MyDouble[][]{
+                {new MyDouble(1d), new MyDouble(2d), new MyDouble(3d)},
+                {new MyDouble(4d), new MyDouble(5d), new MyDouble(6d)},
+                {new MyDouble(7d), new MyDouble(8d), new MyDouble(9d)}});
+        Operations[][] a1 = new Operations[a.getA()][a.getB()];
 
+        MyDouble[][] b = new MyDouble[][]{
+                {new MyDouble(1d), new MyDouble(4d), new MyDouble(7d)},
+                {new MyDouble(2d), new MyDouble(5d), new MyDouble(8d)},
+                {new MyDouble(3d), new MyDouble(6d), new MyDouble(9d)}
+        };
+
+        // act
+        a1 = transposeVisitor.visit(a);
+        MyDouble[][] a2 = new MyDouble[a1.length][a1[0].length];
+
+        // assert
+        for (int i = 0; i < a1.length; i++) {
+            for (int j = 0; j < a1[0].length; j++) {
+                Assert.assertEquals(b[i][j].value,a2[i][j].value,0d);
+            }
+        }
     }
 }
