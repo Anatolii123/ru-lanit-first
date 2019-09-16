66380c8f5f5803a1f374bf03bb7917944bae752e
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-16 13:14:02 +0500

    UnitTest13Commit

---
+++ src/test/java/ru/lanit/factory/MyLongCreatorStub.java
@@ -0,0 +1,15 @@
+package ru.lanit.factory;
+
+public class MyLongCreatorStub extends MyLongCreator {
+    private Operations[][] content;
+
+    public void setContent(Operations[][] content) {
+        this.content = content;
+    }
+
+    @Override
+    protected Operations[][] create(int a, int b) {
+        return content;
+    }
+
+}
---
+++ src/test/java/ru/lanit/factory/MyLongCreatorTest.java
@@ -0,0 +1,12 @@
+package ru.lanit.factory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyLongCreatorTest {
+
+    @Test
+    public void create() {
+    }
+}
