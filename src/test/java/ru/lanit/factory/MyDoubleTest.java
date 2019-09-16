3e3327fd9277553ec234781a086bf9ee5ef9a386
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-16 12:44:24 +0500

    UnitTest9Commit

--- src/test/java/ru/lanit/factory/MyDoubleTest.java
+++ src/test/java/ru/lanit/factory/MyDoubleTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
 
 public class MyDoubleTest {
 
+    /**
+     * Тесты на проверку работоспособности методов
+     */
     @Test
     public void add() {
         // arrange
@@ -45,4 +48,92 @@ public class MyDoubleTest {
         // assert
         Assert.assertEquals(10D,(Double) res.value,0D);
     }
+
+    /**
+     * Тесты на проверку вызываемых исключений
+     */
+    @Test
+    public void addExceptions() {
+        // arrange
+        MyDouble md = new MyDouble(1D);
+        MyDouble that = new MyDouble(5D);
+
+        // act
+        MyDouble res = (MyDouble) md.add(that);
+
+        // assert
+        Assert.assertEquals(6D,(Double) res.value,0D);
+    }
+
+    @Test
+    public void subExceptions() {
+        // arrange
+        MyDouble md = new MyDouble(5D);
+        MyDouble that = new MyDouble(1D);
+
+        // act
+        MyDouble res = (MyDouble) md.sub(that);
+
+        // assert
+        Assert.assertEquals(4D,(Double) res.value,0D);
+    }
+
+    @Test
+    public void multExceptions() {
+        // arrange
+        MyDouble md = new MyDouble(5D);
+        MyDouble that = new MyDouble(2D);
+
+        // act
+        MyDouble res = (MyDouble) md.mult(that);
+
+        // assert
+        Assert.assertEquals(10D,(Double) res.value,0D);
+    }
+
+    /**
+     * Тесты на проверку крайних случаев
+     */
+    @Test
+    public void addExtremeCases() {
+        // arrange
+        MyDouble md = new MyDouble(1D);
+        MyDouble that = new MyDouble(5D);
+
+        // act
+        MyDouble res = (MyDouble) md.add(that);
+
+        // assert
+        Assert.assertEquals(6D,(Double) res.value,0D);
+    }
+
+    @Test
+    public void subExtremeCases() {
+        // arrange
+        MyDouble md = new MyDouble(5D);
+        MyDouble that = new MyDouble(1D);
+
+        // act
+        MyDouble res = (MyDouble) md.sub(that);
+
+        // assert
+        Assert.assertEquals(4D,(Double) res.value,0D);
+    }
+
+    @Test
+    public void multExtremeCases() {
+        // arrange
+        MyDouble md = new MyDouble(5D);
+        MyDouble that = new MyDouble(2D);
+
+        // act
+        MyDouble res = (MyDouble) md.mult(that);
+
+        // assert
+        Assert.assertEquals(10D,(Double) res.value,0D);
+    }
+
+
+
+
 }
