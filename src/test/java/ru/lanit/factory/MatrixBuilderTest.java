66e1cc8af5f289f2974a946d7922662822c1ec0f
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-16 18:55:56 +0500

    UnitTest33Commit

--- src/test/java/ru/lanit/factory/MatrixBuilderTest.java
+++ src/test/java/ru/lanit/factory/MatrixBuilderTest.java
@@ -2,11 +2,14 @@ package ru.lanit.factory;
 
 import jdk.nashorn.internal.objects.Global;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MatrixBuilderTest {
+    private ContentCreator contentCreator;
+    private MyDouble[][] check;
 
     /**
      * Тесты на проверку работоспособности методов
@@ -60,27 +63,28 @@ public class MatrixBuilderTest {
     @Test
     public void toMatrix1x1() {
         // arrange
-        Matrix result = new Matrix();
-        MyDoubleCreatorStub stub = new MyDoubleCreatorStub();
-        stub.setContent(new MyDouble[][]{{new MyDouble(12d)}});
-        MyDouble[][] check = {{new MyDouble(12d)}};
-        MatrixBuilder matrixBuilder = new MatrixBuilder(stub);
-
+        MatrixBuilder matrixBuilder = new MatrixBuilder(contentCreator);
 
         // act
-        result.setMatrix(stub.create(stub.getContent().length, stub.getContent()[0].length));
-        result.setA(stub.getContent().length);
-        result.setB(stub.getContent()[0].length);
-        MyDouble[][] result2 = new MyDouble[result.getA()][result.getB()];
+        Matrix result = matrixBuilder.setA(1).setB(1).toMatrix();
 
         // assert
         for (int i = 0; i < result.getA(); i++) {
             for (int j = 0; j < result.getB(); j++) {
-                result2[i][j] = (MyDouble) result.getMatrix()[i][j];
-                Assert.assertEquals(check[i][j].value,result2[i][j].value,0d);
+                Assert.assertEquals(check[i][j].value, ((MyDouble) result.getMatrix()[i][j]).value);
             }
         }
 
+    }
+
+    @Before
+    public void getContentCreator() {
+        // todo попробовать создангие заглушки вынести в @Before. Пример не наглядные. Прошу что-нибудь подобное сделать в других тестах
+        check = new MyDouble[][]{{new MyDouble(12d)}};
+        MyDoubleCreatorStub stub = new MyDoubleCreatorStub();
+        stub.setContent(check);
+
+        contentCreator = stub;
     }
 
 }
