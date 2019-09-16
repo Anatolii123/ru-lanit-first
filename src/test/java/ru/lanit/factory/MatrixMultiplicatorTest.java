ff8c71a2473e630753f94eba154ca112826296f4
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-16 15:34:44 +0500

    UnitTest20Commit

--- src/test/java/ru/lanit/factory/MatrixMultiplicatorTest.java
+++ src/test/java/ru/lanit/factory/MatrixMultiplicatorTest.java
@@ -4,6 +4,7 @@ import jdk.nashorn.internal.objects.Global;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.lanit.exceptions.AdditionException;
+import ru.lanit.exceptions.MultiplicationException;
 
 import static org.junit.Assert.*;
 
@@ -13,27 +14,30 @@ public class MatrixMultiplicatorTest {
      * Тесты на проверку работоспособности методов
      */
     @Test
-    public void perform() throws AdditionException {
+    public void perform() throws MultiplicationException {
         // arrange
         Matrix operand1 = new Matrix();
         Matrix operand2 = new Matrix();
 
         operand1.setMatrix(new MyDouble[][]{
-                {new MyDouble(1d), new MyDouble(2d), new MyDouble(3d)},
-                {new MyDouble(4d), new MyDouble(5d), new MyDouble(6d)},
-                {new MyDouble(7d), new MyDouble(8d), new MyDouble(9d)}});
+                {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
+                {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
+                {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
 
-        operand2.setMatrix(new MyDouble[][]{{new MyDouble(9d), new MyDouble(8d), new MyDouble(7d)},
+        operand2.setMatrix(new MyDouble[][]{
+                {new MyDouble(9d), new MyDouble(8d), new MyDouble(7d)},
                 {new MyDouble(6d), new MyDouble(5d), new MyDouble(4d)},
                 {new MyDouble(3d), new MyDouble(2d), new MyDouble(1d)}});
 
         // act
-        Operations[][] s = new MatrixSummator().perform(operand1,operand2);
+        Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
+        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                Assert.assertEquals(10d,s[i][j]);
+                s2[i][j] = (MyDouble) s[i][j];
+                Assert.assertEquals(10d,s2[i][j].value,0d);
             }
         }
     }
@@ -58,11 +62,13 @@ public class MatrixMultiplicatorTest {
 
         // act
         Operations[][] s = new MatrixSummator().perform(operand1,operand2);
+        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                Assert.assertEquals(10d,s[i][j]);
+                s2[i][j] = (MyDouble) s[i][j];
+                Assert.assertEquals(10d,s2[i][j].value,0d);
             }
         }
     }
@@ -94,7 +100,7 @@ public class MatrixMultiplicatorTest {
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
                 s2[i][j] = (MyDouble) s[i][j];
-                Assert.assertEquals(Global.Infinity,s2[i][j].value,Global.Infinity);
+                Assert.assertEquals(Global.Infinity,s2[i][j].value,0d);
             }
         }
     }
--- src/test/java/ru/lanit/factory/MatrixSummatorTest.java
+++ src/test/java/ru/lanit/factory/MatrixSummatorTest.java
@@ -23,17 +23,20 @@ public class MatrixSummatorTest {
                 {new MyDouble(4d), new MyDouble(5d), new MyDouble(6d)},
                 {new MyDouble(7d), new MyDouble(8d), new MyDouble(9d)}});
 
-        operand2.setMatrix(new MyDouble[][]{{new MyDouble(9d), new MyDouble(8d), new MyDouble(7d)},
+        operand2.setMatrix(new MyDouble[][]{
+                {new MyDouble(9d), new MyDouble(8d), new MyDouble(7d)},
                 {new MyDouble(6d), new MyDouble(5d), new MyDouble(4d)},
                 {new MyDouble(3d), new MyDouble(2d), new MyDouble(1d)}});
 
         // act
         Operations[][] s = new MatrixSummator().perform(operand1,operand2);
+        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                Assert.assertEquals(10d,s[i][j]);
+                s2[i][j] = (MyDouble) s[i][j];
+                Assert.assertEquals(10d,s2[i][j].value,0d);
             }
         }
     }
@@ -58,11 +61,13 @@ public class MatrixSummatorTest {
 
         // act
         Operations[][] s = new MatrixSummator().perform(operand1,operand2);
+        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                Assert.assertEquals(10d,s[i][j]);
+                s2[i][j] = (MyDouble) s[i][j];
+                Assert.assertEquals(10d,s2[i][j].value,0d);
             }
         }
     }
@@ -94,7 +99,7 @@ public class MatrixSummatorTest {
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
                 s2[i][j] = (MyDouble) s[i][j];
-                Assert.assertEquals(Global.Infinity,s2[i][j].value,Global.Infinity);
+                Assert.assertEquals(Global.Infinity,s2[i][j].value,0d);
             }
         }
     }
