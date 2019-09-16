471ac1f3a245bf56d63f3d82031058fa23bc4926
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-16 17:31:58 +0500

    UnitTest26Commit

--- src/test/java/ru/lanit/factory/MatrixMultiplicatorTest.java
+++ src/test/java/ru/lanit/factory/MatrixMultiplicatorTest.java
@@ -14,7 +14,7 @@ public class MatrixMultiplicatorTest {
      * Тесты на проверку работоспособности методов
      */
     @Test
-    public void perform() throws MultiplicationException {
+    public void multiply_Matrices_Matrix() throws MultiplicationException {
         // arrange
         Matrix operand1 = new Matrix();
         Matrix operand2 = new Matrix();
@@ -43,7 +43,7 @@ public class MatrixMultiplicatorTest {
     }
 
     @Test
-    public void differentDimensionsMultiplicationTest() throws MultiplicationException {
+    public void multiply_DifferentDimensionsMatrices_Matrix() throws MultiplicationException {
         // arrange
         Matrix operand1 = new Matrix();
         Matrix operand2 = new Matrix();
@@ -74,7 +74,7 @@ public class MatrixMultiplicatorTest {
      * Тесты на проверку вызываемых исключений
      */
     @Test(expected = MultiplicationException.class)
-    public void performExceptions() throws MultiplicationException {
+    public void multiply_InconsistentMatrices_MultiplicationException() throws MultiplicationException {
         // arrange
         Matrix operand1 = new Matrix();
         Matrix operand2 = new Matrix();
@@ -105,7 +105,7 @@ public class MatrixMultiplicatorTest {
      * Тесты на проверку крайних случаев
      */
     @Test
-    public void performExtremeCase() throws MultiplicationException {
+    public void multiply_DoubleMaxValueMatrices_Infinity() throws MultiplicationException {
         // arrange
         Matrix operand1 = new Matrix();
         Matrix operand2 = new Matrix();
@@ -129,6 +129,54 @@ public class MatrixMultiplicatorTest {
             for (int j = 0; j < s[0].length; j++) {
                 s2[i][j] = (MyDouble) s[i][j];
                 Assert.assertEquals(Global.Infinity,s2[i][j].value,0d);
+            }
+        }
+    }
+
+    @Test
+    public void multiply_Matrices1x1_Matrix1x1() throws MultiplicationException {
+        // arrange
+        Matrix operand1 = new Matrix();
+        Matrix operand2 = new Matrix();
+
+        operand1.setMatrix(new MyDouble[][]{{new MyDouble(12d)}});
+        operand2.setMatrix(new MyDouble[][]{{new MyDouble(2d)}});
+
+        // act
+        Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
+        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
+
+        // assert
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s[0].length; j++) {
+                s2[i][j] = (MyDouble) s[i][j];
+                Assert.assertEquals(24d,s2[i][j].value,0d);
+            }
+        }
+    }
+
+    @Test
+    public void multiply_DifferentDimensionsMatrices_Matrix1x1() throws MultiplicationException {
+        // arrange
+        Matrix operand1 = new Matrix();
+        Matrix operand2 = new Matrix();
+
+        operand1.setMatrix(new MyDouble[][]{{new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
+
+        operand2.setMatrix(new MyDouble[][]{
+                {new MyDouble(2d)},
+                {new MyDouble(2d)},
+                {new MyDouble(2d)}});
+
+        // act
+        Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
+        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
+
+        // assert
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s[0].length; j++) {
+                s2[i][j] = (MyDouble) s[i][j];
+                Assert.assertEquals(12d,s2[i][j].value,0d);
             }
         }
     }
