e363c366dad8aa5ef98deb6f57d795ed371f8b3d
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-17 16:38:32 +0500

    UnitTest74Commit

--- src/test/java/ru/lanit/factory/MyDoubleCreatorTest.java
+++ src/test/java/ru/lanit/factory/MyDoubleCreatorTest.java
@@ -4,11 +4,11 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.stubbing.OngoingStubbing;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@Ignore
 public class MyDoubleCreatorTest extends AbstractTest {
 
     @Test
@@ -17,25 +17,22 @@ public class MyDoubleCreatorTest extends AbstractTest {
         // arrange
         MyDoubleCreator mdc = new MyDoubleCreator();
         MyDoubleCreator myDoubleCreator = mock(MyDoubleCreator.class);
-        when(myDoubleCreator.create(3,3)).thenReturn(reverseMatrix3x3.getMatrix());
-
-        Double[][] array = {{1D,2D,3D},{4D,5D,6D},{7D,8D,9D}};
-        MyDoubleCreatorStub mdc2 = (MyDoubleCreatorStub) mdc;
-        Operations[][] content = {
-                        {new MyDouble(1d), new MyDouble(2d), new MyDouble(3d)},
-                        {new MyDouble(4d), new MyDouble(5d), new MyDouble(6d)},
-                        {new MyDouble(7d), new MyDouble(8d), new MyDouble(9d)}};
-        mdc2.setContent(content);
+        when(myDoubleCreator.create(3,3)).thenReturn(new MyDouble[][]{
+                {new MyDouble(1d), new MyDouble(2d), new MyDouble(3d)},
+                {new MyDouble(4d), new MyDouble(5d), new MyDouble(6d)},
+                {new MyDouble(7d), new MyDouble(8d), new MyDouble(9d)}});
+        check3x3 = new MyDouble[][]{
+                {new MyDouble(1d), new MyDouble(2d), new MyDouble(3d)},
+                {new MyDouble(4d), new MyDouble(5d), new MyDouble(6d)},
+                {new MyDouble(7d), new MyDouble(8d), new MyDouble(9d)}};
 
         // act
-
-        Operations[][] res = mdc.create(3,3);
-        MyDouble[][] res2 = (MyDouble[][])res;
+        Operations[][] result = myDoubleCreator.create(3,3);
 
         // assert
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                Assert.assertEquals(array[i][j],res2[i][j].value);
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                Assert.assertEquals(((MyDouble) check3x3[i][j]).value,((MyDouble) result[i][j]).value,0d);
             }
         }
     }
