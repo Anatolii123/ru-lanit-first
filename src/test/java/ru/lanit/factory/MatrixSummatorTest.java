ab2c0de3b882da403c5428ab5f20b143b56af877
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-17 13:05:07 +0500

    UnitTest62Commit

--- src/test/java/ru/lanit/factory/MatrixSummatorTest.java
+++ src/test/java/ru/lanit/factory/MatrixSummatorTest.java
@@ -10,13 +10,48 @@ import static org.junit.Assert.*;
 
 public class MatrixSummatorTest {
 
-    private Matrix operand1;
-    private Matrix operand2;
+    private Matrix matrix3x3 = new Matrix();
+    private Operations[][] check3x3;
+    private Matrix matrix2x3 = new Matrix();
+    private Matrix minMatrix = new Matrix();
+    private Matrix maxMatrix = new Matrix();
+    private Operations[][] maxCheck;
+    private Matrix zeroMatrix3x3 = new Matrix();
+    private Matrix reverseMatrix3x3 = new Matrix();
 
     @Before
     public void getMatrices() {
-        operand1 = new Matrix();
-        operand2 = new Matrix();
+        matrix3x3.setMatrix(new MyDouble[][]{
+                {new MyDouble(1d), new MyDouble(2d), new MyDouble(3d)},
+                {new MyDouble(4d), new MyDouble(5d), new MyDouble(6d)},
+                {new MyDouble(7d), new MyDouble(8d), new MyDouble(9d)}});
+        reverseMatrix3x3.setMatrix(new MyDouble[][]{
+                {new MyDouble(9d), new MyDouble(8d), new MyDouble(7d)},
+                {new MyDouble(6d), new MyDouble(5d), new MyDouble(4d)},
+                {new MyDouble(3d), new MyDouble(2d), new MyDouble(1d)}});
+        check3x3 = new MyDouble[][]{
+                {new MyDouble(0d), new MyDouble(0d), new MyDouble(0d)},
+                {new MyDouble(0d), new MyDouble(0d), new MyDouble(0d)},
+                {new MyDouble(0d), new MyDouble(0d), new MyDouble(0d)}};
+        matrix2x3.setMatrix(new MyDouble[][]{
+                {new MyDouble(9d), new MyDouble(8d), new MyDouble(7d)},
+                {new MyDouble(4d), new MyDouble(5d), new MyDouble(6d)}});
+        minMatrix.setMatrix(new MyDouble[][]{
+                {new MyDouble(-Double.MAX_VALUE), new MyDouble(-Double.MAX_VALUE), new MyDouble(-Double.MAX_VALUE)},
+                {new MyDouble(-Double.MAX_VALUE), new MyDouble(-Double.MAX_VALUE), new MyDouble(-Double.MAX_VALUE)},
+                {new MyDouble(-Double.MAX_VALUE), new MyDouble(-Double.MAX_VALUE), new MyDouble(-Double.MAX_VALUE)}});
+        maxMatrix.setMatrix(new MyDouble[][]{
+                {new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE)},
+                {new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE)},
+                {new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE)}});
+        maxCheck = new MyDouble[][]{
+                {new MyDouble(Global.Infinity), new MyDouble(Global.Infinity), new MyDouble(Global.Infinity)},
+                {new MyDouble(Global.Infinity), new MyDouble(Global.Infinity), new MyDouble(Global.Infinity)},
+                {new MyDouble(Global.Infinity), new MyDouble(Global.Infinity), new MyDouble(Global.Infinity)}};
+        zeroMatrix3x3.setMatrix(new MyDouble[][]{
+                {new MyDouble(0d), new MyDouble(0d), new MyDouble(0d)},
+                {new MyDouble(0d), new MyDouble(0d), new MyDouble(0d)},
+                {new MyDouble(0d), new MyDouble(0d), new MyDouble(0d)}});
     }
 
     /**
