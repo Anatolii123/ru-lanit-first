19ff80608bd554c622d0fbdd28ace6c5e4580939
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-17 15:55:23 +0500

    UnitTest73Commit

--- src/test/java/ru/lanit/factory/MyDoubleCreatorTest.java
+++ src/test/java/ru/lanit/factory/MyDoubleCreatorTest.java
@@ -5,14 +5,20 @@ import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @Ignore
-public class MyDoubleCreatorTest {
+public class MyDoubleCreatorTest extends AbstractTest {
 
     @Test
     public void create() {
 
         // arrange
         MyDoubleCreator mdc = new MyDoubleCreator();
+        MyDoubleCreator myDoubleCreator = mock(MyDoubleCreator.class);
+        when(myDoubleCreator.create(3,3)).thenReturn(reverseMatrix3x3.getMatrix());
+
         Double[][] array = {{1D,2D,3D},{4D,5D,6D},{7D,8D,9D}};
         MyDoubleCreatorStub mdc2 = (MyDoubleCreatorStub) mdc;
         Operations[][] content = {
