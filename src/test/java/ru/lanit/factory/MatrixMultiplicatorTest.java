20d39b96d99d5bb972fe6cfec8429c8534c51dbd
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-17 10:51:22 +0500

    UnitTest43Commit

--- src/test/java/ru/lanit/factory/MatrixMultiplicatorTest.java
+++ src/test/java/ru/lanit/factory/MatrixMultiplicatorTest.java
@@ -2,6 +2,7 @@ package ru.lanit.factory;
 
 import jdk.nashorn.internal.objects.Global;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import ru.lanit.exceptions.AdditionException;
 import ru.lanit.exceptions.MultiplicationException;
@@ -10,15 +11,22 @@ import static org.junit.Assert.*;
 
 public class MatrixMultiplicatorTest {
 
+    private Matrix operand1;
+    private Matrix operand2;
+
+    @Before
+    public void getMatrices() {
+        operand1 = new Matrix();
+        operand2 = new Matrix();
+    }
+
     /**
      * Тесты на проверку работоспособности методов
      */
+
     @Test
     public void multiply_Matrices_Matrix() throws MultiplicationException {
         // arrange
-        Matrix operand1 = new Matrix();
-        Matrix operand2 = new Matrix();
-
         operand1.setMatrix(new MyDouble[][]{
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
@@ -31,13 +39,11 @@ public class MatrixMultiplicatorTest {
 
         // act
         Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
-        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                s2[i][j] = (MyDouble) s[i][j];
-                Assert.assertEquals(12d,s2[i][j].value,0d);
+                Assert.assertEquals(12d,((MyDouble) s[i][j]).value,0d);
             }
         }
     }
@@ -45,9 +51,6 @@ public class MatrixMultiplicatorTest {
     @Test
     public void multiply_DifferentDimensionsMatrices_Matrix() throws MultiplicationException {
         // arrange
-        Matrix operand1 = new Matrix();
-        Matrix operand2 = new Matrix();
-
         operand1.setMatrix(new MyDouble[][]{
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
@@ -59,13 +62,11 @@ public class MatrixMultiplicatorTest {
 
         // act
         Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
-        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                s2[i][j] = (MyDouble) s[i][j];
-                Assert.assertEquals(12d,s2[i][j].value,0d);
+                Assert.assertEquals(12d,((MyDouble) s[i][j]).value,0d);
             }
         }
     }
@@ -76,9 +77,6 @@ public class MatrixMultiplicatorTest {
     @Test(expected = MultiplicationException.class)
     public void multiply_InconsistentMatrices_MultiplicationException() throws MultiplicationException {
         // arrange
-        Matrix operand1 = new Matrix();
-        Matrix operand2 = new Matrix();
-
         operand1.setMatrix(new MyDouble[][]{
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
@@ -90,13 +88,11 @@ public class MatrixMultiplicatorTest {
 
         // act
         Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
-        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                s2[i][j] = (MyDouble) s[i][j];
-                Assert.assertEquals(12d,s2[i][j].value,0d);
+                Assert.assertEquals(12d,((MyDouble) s[i][j]).value,0d);
             }
         }
     }
@@ -107,9 +103,6 @@ public class MatrixMultiplicatorTest {
     @Test
     public void multiply_DoubleMaxValueMatrices_Infinity() throws MultiplicationException {
         // arrange
-        Matrix operand1 = new Matrix();
-        Matrix operand2 = new Matrix();
-
         operand1.setMatrix(new MyDouble[][]{
                 {new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE)},
                 {new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE)},
@@ -122,13 +115,11 @@ public class MatrixMultiplicatorTest {
 
         // act
         Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
-        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                s2[i][j] = (MyDouble) s[i][j];
-                Assert.assertEquals(Global.Infinity,s2[i][j].value,0d);
+                Assert.assertEquals(Global.Infinity,((MyDouble) s[i][j]).value,0d);
             }
         }
     }
@@ -136,21 +127,16 @@ public class MatrixMultiplicatorTest {
     @Test
     public void multiply_Matrices1x1_Matrix1x1() throws MultiplicationException {
         // arrange
-        Matrix operand1 = new Matrix();
-        Matrix operand2 = new Matrix();
-
         operand1.setMatrix(new MyDouble[][]{{new MyDouble(12d)}});
         operand2.setMatrix(new MyDouble[][]{{new MyDouble(2d)}});
 
         // act
         Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
-        MyDouble[][] s2 = new MyDouble[s.length][s[0].length];
 
         // assert
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
-                s2[i][j] = (MyDouble) s[i][j];
-                Assert.assertEquals(24d,s2[i][j].value,0d);
+                Assert.assertEquals(24d,((MyDouble) s[i][j]).value,0d);
             }
         }
     }
@@ -158,10 +144,10 @@ public class MatrixMultiplicatorTest {
     @Test
     public void multiply_DifferentDimensionsMatrices_Matrix1x1() throws MultiplicationException {
         // arrange
-        Matrix operand1 = new Matrix();
-        Matrix operand2 = new Matrix();
-
-        operand1.setMatrix(new MyDouble[][]{{new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
+        operand1.setMatrix(new MyDouble[][]{
+                {new MyDouble(2d),
+                        new MyDouble(2d),
+                        new MyDouble(2d)}});
 
         operand2.setMatrix(new MyDouble[][]{
                 {new MyDouble(2d)},
