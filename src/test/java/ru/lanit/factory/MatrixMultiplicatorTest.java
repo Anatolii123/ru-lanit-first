31db20b70cc2793dd84c8d8820f798e3741d0d11
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-17 11:43:27 +0500

    UnitTest53Commit

--- src/test/java/ru/lanit/factory/MatrixMultiplicatorTest.java
+++ src/test/java/ru/lanit/factory/MatrixMultiplicatorTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 
 public class MatrixMultiplicatorTest {
 
-    private Matrix normalMatrix = new Matrix();
+    private Matrix matrix3x3 = new Matrix();
     private Matrix matrix2x3 = new Matrix();
     private Matrix matrix3x2 = new Matrix();
     private Matrix maxMatrix = new Matrix();
@@ -22,17 +22,22 @@ public class MatrixMultiplicatorTest {
 
     @Before
     public void getMatrices() {
-        normalMatrix.setMatrix(new MyDouble[][]{
+        matrix3x3.setMatrix(new MyDouble[][]{
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
-                {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
-                {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
-        matrix2x3.setMatrix(new MyDouble[][]{
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
                 {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
         matrix3x2.setMatrix(new MyDouble[][]{
                 {new MyDouble(2d), new MyDouble(2d)},
                 {new MyDouble(2d), new MyDouble(2d)},
                 {new MyDouble(2d), new MyDouble(2d)}});
+        matrix3x1.setMatrix(new MyDouble[][]{
+                {new MyDouble(2d)},
+                {new MyDouble(2d)},
+                {new MyDouble(2d)}});
+        matrix2x3.setMatrix(new MyDouble[][]{
+                {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)},
+                {new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
+        matrix1x3.setMatrix(new MyDouble[][]{{new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
         maxMatrix.setMatrix(new MyDouble[][]{
                 {new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE)},
                 {new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE), new MyDouble(Double.MAX_VALUE)},
@@ -48,8 +53,8 @@ public class MatrixMultiplicatorTest {
     @Test
     public void multiply_Matrices_Matrix() throws MultiplicationException {
         // arrange
-        Matrix operand1 = normalMatrix;
-        Matrix operand2 = normalMatrix;
+        Matrix operand1 = matrix3x3;
+        Matrix operand2 = matrix3x3;
 
         // act
         Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
@@ -85,7 +90,7 @@ public class MatrixMultiplicatorTest {
     @Test(expected = MultiplicationException.class)
     public void multiply_InconsistentMatrices_MultiplicationException() throws MultiplicationException {
         // arrange
-        Matrix operand1 = normalMatrix;
+        Matrix operand1 = matrix3x3;
         Matrix operand2 = matrix2x3;
 
         // act
@@ -139,12 +144,9 @@ public class MatrixMultiplicatorTest {
     @Test
     public void multiply_DifferentDimensionsMatrices_Matrix1x1() throws MultiplicationException {
         // arrange
-        operand1.setMatrix(new MyDouble[][]{{new MyDouble(2d), new MyDouble(2d), new MyDouble(2d)}});
+        Matrix operand1 = matrix1x3;
 
-        operand2.setMatrix(new MyDouble[][]{
-                {new MyDouble(2d)},
-                {new MyDouble(2d)},
-                {new MyDouble(2d)}});
+        Matrix operand2 = matrix3x1;
 
         // act
         Operations[][] s = new MatrixMultiplicator().perform(operand1,operand2);
