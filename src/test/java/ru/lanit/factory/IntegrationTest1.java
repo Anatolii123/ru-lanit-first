0dd0b8f7a57a29af97d4777b47871e38404c716b
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-18 12:29:15 +0500

    IntegrationTest7Commit

--- src/test/java/ru/lanit/factory/IntegrationTest1.java
+++ src/test/java/ru/lanit/factory/IntegrationTest1.java
@@ -1,18 +1,34 @@
 package ru.lanit.factory;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.lanit.second.Hello;
 
 import java.util.List;
 
-public class IntegrationTest1  {
-
+public class IntegrationTest1 extends AbstractTest {
 
     @Test
     public void readMatrices_OperationsListDevelopment_Output() throws Exception {
         MatrixReader matrixR = new MatrixReader();
         List<Matrix> matrices = matrixR.readMatrix(
                 "C:\\Users\\kusakin\\IdeaProjects\\ru-lanit-first\\src\\main\\resources\\matrix");
+        check3x3 = new MyDouble[][]{
+                {new MyDouble(12d), new MyDouble(12d), new MyDouble(12d)},
+                {new MyDouble(12d), new MyDouble(12d), new MyDouble(12d)},
+                {new MyDouble(12d), new MyDouble(12d), new MyDouble(12d)}};
+
+
+        // assert
+        for (int i = 0; i < matrices.size(); i++) {
+            for (int j = 0; j < result.getA(); j++) {
+                for (int k = 0; k < result.getB(); k++) {
+                    Assert.assertEquals(check3x3[i][j].value,((MyDouble)matrices.get(i).getMatrix()[i][j]).value,0d);
+                }
+            }
+        }
+
+
         List<MatrixOperation> operations = Hello.operationsList(5);
         for (MatrixOperation operation : operations) {
             operation.perform(matrices.get(0), matrices.get(1));
